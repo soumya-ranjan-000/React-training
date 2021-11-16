137d2a124a5c872a8296681f55d40a75bec5a941
soumya-ranjan-000 <rakiraja7751841655@gmail.com>
2021-11-16 15:46:53 +0530

    Add Project "Hello World"

---
+++ demo-app/src/HelloWorld/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <a
+          className="App-link"
+          href="https://github.com/soumya-ranjan-000/React-training"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         My Github Documents
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
